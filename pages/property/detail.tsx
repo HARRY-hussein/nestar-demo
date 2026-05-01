2c18b15ad993b1c2335a1ae134901e25358cddda
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-05-01 15:46:28 +0900

    feat: integration Header and Footer via Layouts

---
+++ pages/property/detail.tsx
@@ -0,0 +1,9 @@
+import { Container, Stack } from "@mui/material";
+import PropertyList from ".";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+
+const PropertyDetail= () => {
+  return <Container>PROPERTY DETAIL</Container>
+};
+
+export default withLayoutBasic(PropertyDetail);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,7 +1,13 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
-const PropertyList = () => {
-  return <div>PROPERTY LIST</div>;
+const PropertyList: NextPage = () => {
+  return (
+    <>
+      <Container>PROPERTY LIST</Container>
+    </>
+  );
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
