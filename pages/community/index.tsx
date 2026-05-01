df745979763721141a6152fe5a0253f8601bcf25
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-05-01 14:44:26 +0900

    feat: organize and test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Community = () => {
+  return <div>COMMUNITY LIST</div>;
+};
+
+export default Community;
---
+++ pages/property/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const PropertyList = () => {
+  return <div>PROPERTY LIST</div>;
+};
+
+export default PropertyList;
