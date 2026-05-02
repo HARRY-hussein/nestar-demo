d86173d23bfa0746c0da6443d52f3248f29676a2
HARRY-hussein <rahmatovhusanjon@icloud.com>
2026-05-02 09:49:32 +0900

    feat: SASS integration

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -6,7 +6,7 @@ const Community: NextPage = () => {
   const [title, setTitle] = useState<string>("hello");
   return (
     <div>
-      COMMUNITY{" "}
+      COMMUNITY
       <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
         PressMe
       </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
-import { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <>
-      <Container>
-        <Stack flexDirection={"column"}>
-          <Box>Popular Properties</Box>
-          <Box>Top Agents</Box>
-          <Box>Top Properties</Box>
-          <Box>Events</Box>
+    <Stack>
+      <Stack flexDirection={"column"}>
+        <Stack>
+          <Stack className="container">Popular Properties</Stack>
         </Stack>
-      </Container>
-    </>
+        <Stack>
+          <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Properties</Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
-export default withLayoutMain(Home);
+export default Home;
